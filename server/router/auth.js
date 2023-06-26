4eacc4a571771ef4626dbf3b6db2bda61a5ec0b3
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-06-26 14:56:30 +0530

    till v-10) post using async-await

--- server/router/auth.js
+++ server/router/auth.js
@@ -7,7 +7,7 @@ router.get("/", (req, res) => {
   console.log("hello world");
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   console.log("from auth.js", req.body);
 
   // const name = req.body.name;// this is a headtack, repeatitive way to for taking valuees of input
@@ -18,25 +18,22 @@ router.post("/register", (req, res) => {
     return res.status(422).json({ error: "Plz fill the field properly" });
   }
 
-  User.findOne({ email: email })
-    .then((userExist) => {
-      if (userExist) {
-        return res.status(422).json({ error: "Email already Exist " });
-      }
-      //if user is not exist, means they haven't register prior so we need to create there document in the collection
-      const user = new User({ name, email, phone, work, password, cpassword }); //19.10
+  try {
+    const userExist = await User.findOne({ email: email });
+    //if user is not exist, means they haven't register prior so we need to create there document in the collection
+    const user = new User({ name, email, phone, work, password, cpassword }); //19.10
 
-      //after geting the user's document from above we need to save it in the collections
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({ message: "user registered successfuly" });
-        })
-        .catch((err) =>
-          res.status(500).json({ error: "Failed to registered" })
-        );
-    })
-    .catch((err) => console.log(err));
+    //after geting the user's document from above we need to save it in the collections
+    const userRegister = await user.save();
+
+    if (userRegister) {
+      res.status(201).json({ message: "user registered successfuly" });
+    } else {
+      res.status(500).json({ error: "Failed to registered" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
