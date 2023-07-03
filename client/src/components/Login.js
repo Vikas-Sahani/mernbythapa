d4c63abee5691ca3a22d5db09cd8633acc946845
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-07-03 22:21:47 +0530

    v-22) Login & registration is done till now

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Login() {
-  return <div className="mt-20">Login</div>;
+  return (
+    <div className="mt-20">
+      <div className="ml-10">
+        <h1>Login</h1>
+        <form>
+          <br />
+          <input type="Email" placeholder="Your Email" className="border-b-2" />
+          <br />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            className="border-b-2"
+          />
+          <br />
+          <br />
+          <button className="bg-blue-700 text-white rounded-full w-20">
+            Login{" "}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,7 +1,49 @@
 import React from "react";
 
 function Register() {
-  return <div className="mt-20">Register</div>;
+  return (
+    <div className="mt-20">
+      <br />
+      <br />
+      <div className="ml-10">
+        <h1>Registeration of User</h1>
+        <form>
+          <input type="text" className="border-b-2" placeholder="Your Name" />
+          <br />
+          <br />
+          <input type="email" className="border-b-2" placeholder="Your Email" />
+          <br />
+          <br />
+          <input
+            type="number"
+            className="border-b-2"
+            placeholder="Mobile Number"
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            className="border-b-2"
+            placeholder="Your Profession"
+          />
+          <br />
+          <br />
+          <input
+            type="password"
+            className="border-b-2"
+            placeholder="Password"
+          />
+          <br />
+          <br />
+          <input
+            type="password"
+            className="border-b-2"
+            placeholder="Confirm Password"
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
