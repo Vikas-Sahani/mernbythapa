ad0cef77cc1018e3560557f95d288e1100c89bb1
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-07-03 21:45:02 +0530

    v-19) React-Router-Dom & components(Home, about, contact, login, register) basic coded

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
-// import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Contact from "./components/Contact";
 
 function App() {
-  return <h1 className="text-9xl">Hello world</h1>;
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
