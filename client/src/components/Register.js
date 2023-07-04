ada76bd80c4fd64e659ce72cb8a91c96664628d6
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-07-04 10:22:24 +0530

    v-28) till now we have stored our frontend Reigistration data in a ObjState

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Register() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    work: "",
+    password: "",
+    cpassword: "",
+  });
+  console.log(user);
+
+  let inpName, inpValue;
+
+  const handleInputs = (e) => {
+    inpName = e.target.name;
+    inpValue = e.target.value;
+
+    setUser({ ...user, [inpName]: inpValue });
+  };
   return (
     <div className="mt-20">
       <br />
@@ -8,14 +26,31 @@ function Register() {
       <div className="ml-10">
         <h1>Registeration of User</h1>
         <form>
-          <input type="text" className="border-b-2" placeholder="Your Name" />
+          <input
+            type="text"
+            name={inpName}
+            value={inpValue}
+            onChange={handleInputs}
+            className="border-b-2"
+            placeholder="Your Name"
+          />
           <br />
           <br />
-          <input type="email" className="border-b-2" placeholder="Your Email" />
+          <input
+            type="email"
+            name={inpName}
+            value={inpValue}
+            onChange={handleInputs}
+            className="border-b-2"
+            placeholder="Your Email"
+          />
           <br />
           <br />
           <input
             type="number"
+            name={inpName}
+            value={inpValue}
+            onChange={handleInputs}
             className="border-b-2"
             placeholder="Mobile Number"
           />
@@ -23,6 +58,9 @@ function Register() {
           <br />
           <input
             type="text"
+            name={inpName}
+            value={inpValue}
+            onChange={handleInputs}
             className="border-b-2"
             placeholder="Your Profession"
           />
@@ -30,6 +68,9 @@ function Register() {
           <br />
           <input
             type="password"
+            name={inpName}
+            value={inpValue}
+            onChange={handleInputs}
             className="border-b-2"
             placeholder="Password"
           />
@@ -37,6 +78,9 @@ function Register() {
           <br />
           <input
             type="password"
+            name={inpName}
+            value={inpValue}
+            onChange={handleInputs}
             className="border-b-2"
             placeholder="Confirm Password"
           />
