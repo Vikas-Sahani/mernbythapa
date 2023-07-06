b5d1fa72cfc72d3ebc29e3a81ce85b3010774347
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-07-06 11:33:24 +0530

    v-32) & About.js UI completed till now

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const User = require("../model/userSchema");
+
+const Authenticate = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwtoken; //getting the stored token
+    const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
+
+    const rootUser = await User.findOne({
+      _id: verifyToken._id,
+      "tokens.token": token,
+    });
+
+    if (!rootUser) {
+      // if user is not found
+      throw new Error("User not Found");
+    }
+
+    //if user is find
+    req.token = token;
+    req.rootUser = rootUser;
+    req.userId = rootUser._id;
+
+    next();
+  } catch (err) {
+    res.status(401).send("Unauthorized: No token provided");
+    console.log(err);
+  }
+};
+
+module.exports = Authenticate;
